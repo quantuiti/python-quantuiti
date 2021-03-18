3261fd54297e205dc59d4bd2095198c0b9bc8cab
dmuraco3 <dylanjmuraco@gmail.com>
2021-03-18 14:17:15 -0400

    third doc fix

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ release = version
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    ’sphinx.ext.napoleon’
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
