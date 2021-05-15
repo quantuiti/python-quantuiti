88300a8f713606a86907a313cdbce3aea8fcda23
dmuraco3 <dylanjmuraco@gmail.com>
2021-05-15 16:11:07 -0400

    Updated ema indicator so that it uses up less compute power as dataset gets bigger.

--- quantuiti/indicators/_ema.py
+++ quantuiti/indicators/_ema.py
@@ -11,21 +11,29 @@ def ema(self, N):
     dependent = 'sma_' + str(N)
 
     if not self.backtest:
-        self.sma(N)
-        temp=[]
-        for index, row in self.data.iterrows():
-            if np.isnan(row[dependent]):
-                temp.append(row[dependent])
-            else:
-                if np.isnan(temp[-1]):
-                    ema = (self.data['Close'][index] - self.data[dependent][index]) * (2 / (N + 1)) + self.data[dependent][index]
+        if N < len(self.data):
+            self.sma(N)
+            temp=[]
+            for index, row in self.data.iterrows():
+                if np.isnan(row[dependent]):
+                    temp.append(row[dependent])
                 else:
-                    ema = (self.data['Close'][index] - temp[-1]) * (2 / (N + 1)) + temp[-1]
-                
-                temp.append(ema)
-        
-        self.data[name] = temp
-        return self.data[name][self.index]
+                    if np.isnan(temp[-1]):
+                        ema = (self.data['Close'][index] - self.data[dependent][index]) * (2 / (N + 1)) + self.data[dependent][index]
+                    else:
+                        ema = (self.data['Close'][index] - temp[-1]) * (2 / (N + 1)) + temp[-1]
+                    
+                    temp.append(ema)
+            
+            self.data[name] = temp
+            return temp[self.index]
+        elif len(self.data) > N:
+            ema = (self.data['Close'][self.index-1] - self.data[name][self.index-1]) * (2 / (N + 1)) + self.data[name][self.index-1]
+
+            temp = self.data[name].append(ema)
+            self.data[name] = temp
+            return ema
+
     elif self.backtest:
         try:
             return self.data[name][self.index]
